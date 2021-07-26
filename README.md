e85e8979bf26e947402f0b031e55c0a4ba0a4a71
Ronison Vichi de Matos <ronison.matos@neogrid.com>
2021-07-25 21:47:28 -0300

    HashSet e TreeSet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PadawanInJava
 Aprendiz da Força
+
+Para se tornar um Jedi Java, trinar você deve! - Yoda
+
+Que a força Java esteja comigo!
---
+++ src/colecoes/treeSet.java
@@ -0,0 +1,25 @@
+package colecoes;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class treeSet {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        SortedSet<String> alfabeto = new TreeSet<>();
+
+        alfabeto.add("A");
+        alfabeto.add("R");
+        alfabeto.add("T");
+        alfabeto.add("C");
+        alfabeto.add("B");
+
+        for (String listar : alfabeto) {
+            System.out.println(listar);
+        }
+
+    }
+
+}
